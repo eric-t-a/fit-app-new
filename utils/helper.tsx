75d0a224e9de812203d657154b226ec42fcfa9d9
Eric Agostinho <ericagostinho@Erics-MacBook-Air.local>
2025-05-12 15:54:07 -0300

    refining calories count

--- utils/helper.tsx
+++ utils/helper.tsx
@@ -66,25 +66,25 @@ function getSpeedThroughUSPace(pace: number){
 }
 
 function getMets(speed: number){ // m/s
-    if(speed > getSpeedThroughUSPace(6)) return 15;
-    if(speed > getSpeedThroughUSPace(7)) return 13;
-    if(speed > getSpeedThroughUSPace(8)) return 11.5;
-    if(speed > getSpeedThroughUSPace(9)) return 10;
-    if(speed > getSpeedThroughUSPace(10)) return 9;
-    if(speed > getSpeedThroughUSPace(12)) return 7;
-    if(speed > getSpeedThroughUSPace(13.5)) return 5.5;
-    if(speed > getSpeedThroughUSPace(15)) return 4;
-    if(speed > getSpeedThroughUSPace(17)) return 2.8;
-    if(speed > getSpeedThroughUSPace(20)) return 2.3;
-    if(speed > getSpeedThroughUSPace(30)) return 1.5;
-    return 1;
+    if(speed > getSpeedThroughUSPace(6)) return 16;
+    if(speed > getSpeedThroughUSPace(7)) return 14;
+    if(speed > getSpeedThroughUSPace(8)) return 12.5;
+    if(speed > getSpeedThroughUSPace(9)) return 11;
+    if(speed > getSpeedThroughUSPace(10)) return 10;
+    if(speed > getSpeedThroughUSPace(12)) return 7.5;
+    if(speed > getSpeedThroughUSPace(13.5)) return 6.5;
+    if(speed > getSpeedThroughUSPace(15)) return 5;
+    if(speed > getSpeedThroughUSPace(17)) return 3.8;
+    if(speed > getSpeedThroughUSPace(20)) return 3.3;
+    if(speed > getSpeedThroughUSPace(30)) return 2.5;
+    return 2;
 }
 
 function calculateCalories(distance: number, time: number, weight: number){ // meters, seconds and kg
     const speed = distance / time;
     const mets = getMets(speed);
 
-    return mets * weight * time / 3600;
+    return mets * weight * time * 3.5 / (60 * 200);
 }
 function getPace(distance: number, start_time: Date){
     if(!start_time || !distance) return '00:00';
